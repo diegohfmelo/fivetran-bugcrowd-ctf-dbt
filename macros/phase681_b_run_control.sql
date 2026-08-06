520b589a3b5ad5c0172c988d86da89a238d171b8
diegohfmelo <diiegoo.melo@hotmail.com>
2026-08-06 00:22:38 -0300

    Align Phase 681 marker with DBT env validation

--- macros/phase681_b_run_control.sql
+++ macros/phase681_b_run_control.sql
@@ -1,6 +1,6 @@
 {% macro phase681_b_run_control() %}
   {% if execute %}
-    {% set marker = env_var('PHASE681_MARKER', 'PHASE681_MARKER_UNSET') %}
+    {% set marker = env_var('DBT_PHASE681_MARKER', 'PHASE681_MARKER_UNSET') %}
     {% set result = run_query('select current_database() as database_name, current_user as user_name') %}
     {% if result is not none and (result.rows | length) == 1 %}
       {{ exceptions.raise_compiler_error('PHASE681_B_EXECUTION_MARKER=' ~ marker ~ '|QUERY_OK=true') }}
